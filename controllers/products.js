29c08dfd86e5d9321f7f6cd5b6a57aa1351771da
Richie <richiemugambi4@gmail.com>
2021-09-08 14:26:58 +0300

    finished filters

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Products=require('../model/products')
 //impo
 const getAllproducts=async (req,res)=>{
-    const {featured,company,name,sort,fields}=req.query
+    const {featured,company,name,sort,fields,numericFilters}=req.query
     const queryObject={}
     if(featured){
        queryObject.featured=featured==='true'?true:false;
@@ -12,6 +12,28 @@ const getAllproducts=async (req,res)=>{
     if(name){
         queryObject.name={ $regex: name ,$options:'i'}
     }
+    if(numericFilters){
+        const operatorMap={
+            '<':'$lt',
+            '<=':'$lte',
+            '=':'$eq',
+            '>':'$gt',
+            '>=':'$gte'
+        }
+        const regEx=/\b(<|>|<=|>=|=)\b/g
+        //dynamic javascript object
+        let filters= numericFilters.replace(regEx,(match)=>`-${operatorMap[match]}-`)
+        const options= ['price','rating']
+        filters=filters.split(',').forEach((item)=>{
+            const[fields,operator,value]=item.split('-')
+            if(options.includes(fields)){
+                queryObject[fields]={[operator]:Number(value)}
+            }
+                }
+            )
+        console.log(filters)
+    }
+    console.log(queryObject)
     let result= Products.find(queryObject)
     //sort
     if(sort){
@@ -34,10 +56,11 @@ const getAllproducts=async (req,res)=>{
     const products= await result
     res.status(200).json({products,nbHits:products.length}) 
 }
+//
 
 
 const getAllproductsStatic=async (req,res)=>{
-    const products= await Products.find().sort('name').limit(2).skip(0)
+    const products= await Products.find({price:{$gt: 30}}).sort('price').limit(15).skip(0)
     res.status(200).json({products,nbHits:products.length})
 }
 
